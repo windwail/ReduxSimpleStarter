9fbe0e39c3537a3de1334745f88abf1d02b1da73
windwail <windwail@mail.ru>
2017-10-05 21:43:19 +0300

    123

--- src/index.js
+++ src/index.js
@@ -13,20 +13,30 @@ class App extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {videos: []};
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
 
 
         YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-            this.setState({videos});
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
         });
 
     }
+
 
     render() {
         return (
             <div>
                 <SearchBar/>
-                <VideoList videos={this.state.videos}/>
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList
+                    onVideoSelect={selectedVideo=>this.setState({selectedVideo})}
+                    videos={this.state.videos}/>
             </div>
         );
     }
